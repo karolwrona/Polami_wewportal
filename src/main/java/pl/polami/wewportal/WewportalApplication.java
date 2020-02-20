a56473ec253499f06e151bd96ab4eb76dbade5db
karolwrona <karol.mikolaj.wronski@gmail.com>
2020-02-20 21:19:34 +0100

    Można generować .war, który działa na zwenętrzym tomcacie.

--- src/main/java/pl/polami/wewportal/WewportalApplication.java
+++ src/main/java/pl/polami/wewportal/WewportalApplication.java
@@ -2,12 +2,18 @@ package pl.polami.wewportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WewportalApplication {
+public class WewportalApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WewportalApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WewportalApplication.class);
+    }
 }
